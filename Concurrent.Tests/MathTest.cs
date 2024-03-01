045b61fa80dd11ccb956766c6132a30c6700e92c
Chilli <chilli@thechilli.dev>
2024-03-01 01:25:17 +0100

    Initial commit

---
+++ Concurrent.Tests/MathTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace Concurrent.Tests;
+
+[TestFixture]
+[TestOf(typeof(Math))]
+public class MathTest
+{
+
+    [Test]
+    public void TestAddTwoNumbers()
+    {
+        var result = Math.AddTwoNumbers(1, 2);
+        Assert.That(result, Is.EqualTo(3));
+    }
+}
