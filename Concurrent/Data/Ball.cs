6801a48dc031a97b153e00ef318e9de017c5eabc
Łukasz Moskwin <lukasz_moskwin@o2.pl>
2024-05-14 18:08:41 +0200

    Data

---
+++ Concurrent/Data/Ball.cs
@@ -0,0 +1,22 @@
+﻿namespace Concurrent.Data
+{
+    public class Ball
+    {
+        public double PositionX { get; set; }
+        public double PositionY { get; set; }
+        public double Radius { get; set; }
+
+        public Ball(double pozycjaX, double pozycjaY, double promien)
+        {
+            PositionX = pozycjaX;
+            PositionY = pozycjaY;
+            Radius = promien;
+        }
+
+        public void UpdatePosition(double newPositionX, double newPositionY)
+        {
+            PositionX = newPositionX;
+            PositionY = newPositionY;
+        }
+    }
+}
---
+++ Concurrent/Data/BallRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace Concurrent.Data
+{
+    public class BallRepository
+    {
+        private List<Ball> _balls;
+        public BallRepository()
+            {
+                _balls = new List<Ball>();
+            }
+        public List<Ball> getBalls()
+        {
+            return _balls.ToList();
+        }
+        public void AddBall(Ball ball)
+        {
+            _balls.Add(ball);
+        }
+        public void RemoveBall(Ball ball)
+        {
+            _balls.Remove(ball);
+        }
+    }
+}
